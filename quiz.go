984b9b1430f48a6093c4160df98a5ef7ce6ba81f
osparea0 <davearose@me.com>
2024-07-30 13:58:24 +0200

    changed some structs for better app design and logic

--- quiz.go
+++ quiz.go
@@ -2,25 +2,37 @@ package main
 
 type Quizzer interface {
 	Generate() (error, questions []Question)
-	Grade(submittedQuestions []Question) (error, grade float64)
+	Grade(submittedQuestions []Question) (error, grade float32)
+	PercentageOverall() (error, float32)
 }
 
+type Games struct {
+	Quizzes []Quiz
+}
+
+// Quiz struct holds the fields for a single quiz and implements the Quizzer interface
 type Quiz struct {
 	Id        int64      `json:"id"`
+	Players   []Player   `json:"players"`
 	Questions []Question `json:"questions"`
 }
 
+// Player is the struct to hold the history of a single players past quizzes
 type Player struct {
-	Name    string `json:"name"`
-	Id      int64  `json:"id"`
-	Quizzes []Quiz `json:"quizzes"`
+	Name    string     `json:"name"`
+	Id      int64      `json:"id"`
+	QuizId  int64      `json:"quiz_id"`
+	Answers []Question `json:"answers"`
+	Score   float32    `json:"score"`
 }
 
+// Question struct holds a single question and all of it's answers
 type Question struct {
 	Question string    `json:"question"`
 	Answers  []Answers `json:"answers"`
 }
 
+// Answer holds all possible answers for a single question and is comprised of Answer structs
 type Answers struct {
 	Answer1 Answer `json:"answer1,omitempty"`
 	Answer2 Answer `json:"answer2,omitempty"`
@@ -28,12 +40,14 @@ type Answers struct {
 	Answer4 Answer `json:"answer4,omitempty"`
 }
 
+// Answer is a single answer and whether it is true or not
 type Answer struct {
 	Answer string `json:"answer"`
 	IsTrue bool   `json:"is_true"`
 }
 
-func (a *Answers) HasOnlyOneTrue() bool {
+// HasOnlyOneTrue ensures there is only one true answer
+func (a Answers) HasOnlyOneTrue() bool {
 	counter := 0
 
 	if a.Answer1.IsTrue {
